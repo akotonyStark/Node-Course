15a539347ae2d8530f0988f694e44a96f9606243
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-06-12 20:29:50 +0000

    Update 13-functionalProgramming.js

--- playground/13-functionalProgramming.js
+++ playground/13-functionalProgramming.js
@@ -37,3 +37,17 @@ const insideFn = error => {
 // They can be sent to other functions as arguments
 insideFn(message => console.log(message));
 
+
+//They can also be returned from other functions, just like variables:
+const createScream = function(logger) {
+  return function(message) {
+    logger(message.toUpperCase() + "!!!");
+  };
+};
+
+const scream = createScream(message => console.log(message));
+
+scream("functions can be returned from other functions");
+scream("createScream returns a function");
+scream("scream invokes that returned function");
+
