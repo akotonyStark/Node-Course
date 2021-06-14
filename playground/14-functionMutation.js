b68e7a0b2141626d249e11a8f18d22c49eef590a
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-06-14 10:02:05 +0000

    Create 14-functionMutation.js

---
+++ playground/14-functionMutation.js
@@ -0,0 +1,23 @@
+const lawn_color = {
+  title : 'lawn',
+  color:  'green',
+  rating: 0
+}
+
+function rateColor(color, rating){
+  color.rating = rating
+  return color
+}
+
+console.log(rateColor(lawn_color, 5).rating); //5
+console.log(lawn_color.rating); //5
+
+
+//to make this object immutable
+function rateColor(color, rating){
+  return Object.assign({}, color, {rating:rating})
+}
+
+console.log(rateColor(lawn_color, 5).rating); //5
+console.log(lawn_color.rating); //0
+  
