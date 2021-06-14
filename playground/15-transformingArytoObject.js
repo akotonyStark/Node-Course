ac2321d6532d1b9949ac7c685532802c2c235105
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-06-14 12:58:04 +0000

    Create 15-transformingArytoObject.js

---
+++ playground/15-transformingArytoObject.js
@@ -0,0 +1,14 @@
+const schools = {
+  Legon: 10,
+  KNUST: 8,
+  UCC :6
+}
+
+const schoolArray = Object.keys(schools).map(key =>({
+  name:key,
+  wins:schools[key]
+}));
+                                             
+               
+                                             
+                                             
